69d8b103c78cfa96a5d04877dcd8bff1b00632ac
박총명 <qkrri56@naver.com>
2025-08-25 17:22:52 +0900

    [ENH] 로그인 폼 구현

--- project/src/routes/auth/Login.jsx
+++ project/src/routes/auth/Login.jsx
@@ -1,16 +1,27 @@
 import { auth } from '@/api/auth.js'
+import {useState} from "react";
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const handleLogin = async () => {
     const response = await auth.login({
-      email: 'a@a.com',
-      password: '123456',
+      email: email,
+      password: password,
     })
     console.log(response)
   }
 
   return (
     <div>
+      <div>
+        <label htmlFor="">이메일</label>
+        <input onChange={(e) => setEmail(e.target.value)} type="text"/>
+      </div>
+      <div>
+        <label htmlFor="">비밀번호</label>
+        <input onChange={(e) => setPassword(e.target.value)} type="password"/>
+      </div>
       <button type="button" onClick={handleLogin}>
         로그인
       </button>
